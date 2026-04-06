5ef2a5d22b361d77cddc89fc80d4a1d7527fc37b
sukumarlightningleap <sukumar@lightningleap.org>
2026-04-06 16:54:21 +0530

    fix: remove prisma.config.ts and rely on schema.prisma

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  engine: 'library',
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-});
